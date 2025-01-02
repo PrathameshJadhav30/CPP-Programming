100180d17404c964110164c92990401f74924061
Prathamesh <prathameshaj2004@gmail.com>
2025-01-02 21:09:33 +0530

    Changes

---
+++ 03 - InputOutput/TypeConversion.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+        // Initializing variables
+        int x= 19, y=2;
+        float div;
+        // type conversion
+        div=float(x)/y; 
+        cout << "The output : " << div;
+        return 0;
+}
