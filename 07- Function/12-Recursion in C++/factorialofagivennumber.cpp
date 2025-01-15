aea9eee0316ea86db62fbf12e9213e6fed8721c7
Prathamesh <prathameshaj2004@gmail.com>
2025-01-15 23:17:38 +0530

    C++

---
+++ 07- Function/12-Recursion in C++/factorialofagivennumber.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int n)
+{
+  if (n == 0)
+    return 1;
+  else
+    return n * factorial(n - 1);
+}
+
+int main()
+{
+  int num;
+  cout << "Enter a number: "; cin >> num;
+
+  int result = factorial(num);
+  cout << "The factorial of " << num << " is " << result << endl;
+
+  return 0;
+}
