be5ca01b9cc4796407c49600ce00f4e277a8cfd1
Prathamesh <prathameshaj2004@gmail.com>
2025-01-23 00:05:42 +0530

    C++

---
+++ 11- STL in C++/Sort in C++ STL/SortinginAscendingOrder.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm> // For sort function
+
+int main() {
+    std::vector<int> arr; // Specify the type as int
+    arr.push_back(34);
+    arr.push_back(70);
+    arr.push_back(20);
+    arr.push_back(56);
+    arr.push_back(1);
+    
+    std::cout << "The elements of the array before sorting: ";
+    for (size_t i = 0; i < arr.size(); i++) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+
+    // Sort function to sort the elements
+    std::sort(arr.begin(), arr.end());   
+    std::cout << "The elements of the array after sorting: ";
+    for (size_t i = 0; i < arr.size(); i++) {
+        std::cout << arr[i] << " ";
+    }
+   
+    return 0;
+}
